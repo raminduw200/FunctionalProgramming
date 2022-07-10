5ae983b4b1234df28264028433d3269bff5be450
raminduw200 <raminduw200@gmail.com>
2022-07-10 10:53:47 +0530

    Add Tut2 answers

---
+++ Tutorial 2/q_4_b.scala
@@ -0,0 +1,28 @@
+
+/*
+Rs 15.00 per ticket = 120 people attend 
+Rs. 10 = attendance 140
+Rs. 20 = attendance 100. 
+performance costs = Rs.500. 
+attendee costs = 3 Rupees. 
+*/
+object Question4 {
+    val performanceCost = 500;
+    val attendeeCost = 3;
+    def main(args: Array[String]) {
+        println("The best ticket price is " + bestTicketPrice(10, 140, 0));
+    }
+    def bestTicketPrice(
+                        ticketPrice: Float, 
+                        attendance: Int,
+                        oldProfit: Float): Float = {
+                            val totalCost = performanceCost + (attendance * attendeeCost);
+                            val newProfit = (ticketPrice * attendance) - totalCost;
+
+                            if (oldProfit < newProfit) {
+                                bestTicketPrice(ticketPrice + 5, attendance - 20, newProfit);
+                            } else {
+                                ticketPrice - 5;
+                            }
+                        }
+}
